3f227e60be46591d2022a19fc3c350f8916aa1f1
Ashutosh yadav <49818462+ashu9972ya@users.noreply.github.com>
2022-09-06 10:35:27 +0530

    Add files via upload

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -6,15 +6,15 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Appbar() {
-  const [cartOpen, setCartOpen] = useState(false);
+  // const [cartOpen, setCartOpen] = useState(false);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton onClick={() => setCartOpen(true)} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton  edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <RestaurantIcon/>
           </IconButton>
           <IconButton  color="inherit" aria-label="menu" sx={{ mr: 2 }}>
