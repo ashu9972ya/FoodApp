780fb78052f5598976ec91edd6fc7cfc30ab9ef2
Ashutosh yadav <49818462+ashu9972ya@users.noreply.github.com>
2022-09-06 09:57:19 +0530

    Add files via upload

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -5,14 +5,20 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useState } from "react";
 
 export default function Appbar() {
+  const [cartOpen, setCartOpen] = useState(false);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton onClick={() => setCartOpen(true)} edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <RestaurantIcon/>
+          </IconButton>
+          <IconButton  color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <ShoppingCartIcon/>
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
             Food's Restaurant
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,16 +3,28 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Feeds from "./data/Feeds.json";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { useState } from 'react';
+import styled from "styled-components";
+import { tooltipClasses } from '@mui/material';
 
 
+
+const StyledMenu = styled.div`
+  height: 100%;
+  width: 67%;
+  margin: 65px 0 0 20px;
+  background: #fff;
+  padding: 5px 20px 20px 20px;
+  display: "flex";
+`;
 export default function Menu() {
-    const[item, setItem]=useState(0)
-    
+  const[value, setValue]=useState(1) 
   return (
+    <StyledMenu>
     <>
 {Feeds.map((data,key)=>{
     return(
@@ -30,10 +42,16 @@ export default function Menu() {
           <Typography variant="body2" color="text.secondary">
             price: {data.price}
           </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Total={value}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Cost(INR):100
+          </Typography>
         </CardContent>
         <CardActions>
-          <Button onClick={()=>{setItem(item+1)}} style={{fontsize:50}} size="Larger">+</Button>
-          <Button onClick={()=>{setItem(Math.max(item-1,0))}} size="Larger">-</Button>
+         <button> <AddIcon fontSize="small"  onClick={()=>{setValue(value+1)}} disabled={value===1}  /></button>
+         <button><RemoveIcon fontSize="small"  onClick={()=>{setValue(Math.max(value-1,0))}} /></button>
         </CardActions>
       </Card>
 
@@ -41,5 +59,6 @@ export default function Menu() {
 })}
    
     </>
+    </StyledMenu>
   );
 }
